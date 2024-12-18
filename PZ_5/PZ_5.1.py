ae81ac07e147e0ce8573cdd1bf1fae308ded3375
chebupa <aristarhsakulin@gmail.com>
2024-12-18 17:30:47 +0300

    pz_5 modified

--- PZ_5/PZ_5.1.py
+++ PZ_5/PZ_5.1.py
@@ -2,13 +2,18 @@
 # Составить функцию, которая выполнит суммирования числового ряда.
 
 # Решение
-def sum_row(n: int):
-    return sum(range(1, n + 1))
+
+def sum_row(start, end):
+    total = 0
+    for num in range(start, end + 1):
+        total += num
+    return total
 
 # Пример использования
 while True:
     try:
-        N = int(input("Введите число N: "))
-        print(f"Сумма чисел от 1 до {N}: {sum_row(N)}")
+        num1 = int(input("Введите число 1: "))
+        num2 = int(input("Введите число 2: "))
+        print(f"Сумма чисел от {num1} до {num2}: {sum_row(num1, num2)}")
     except ValueError:
         print("Неверный ввод")
